651118f00ea5c19e84f4ae3b3433fa8d7cd51572
Adidave2009 <64607962+Adidave2009@users.noreply.github.com>
2020-05-02 18:29:43 -0400

    Adding Square Root Calculator for calculation of square root.

--- Percentage-Calculator/Percentage-Calculator/Program.cs
+++ Percentage-Calculator/Percentage-Calculator/Program.cs
@@ -4,19 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Percentage_Calculator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Decimal totalValue = Convert.ToDecimal(args[0]);
-            Decimal percentage = Convert.ToDecimal(args[1]);
+            //Decimal totalValue = Convert.ToDecimal(args[0]);
+            //Decimal percentage = Convert.ToDecimal(args[1]);
+            //getPercentage(totalValue, percentage);
+
+
+            Double squreValue = Convert.ToDouble(args[0]);
+            getSquareRoot(squreValue);
+
+
+        }
+
+
+        static void getPercentage(Decimal totalValue, Decimal percentage)
+        {
             Decimal result = 0;
             result = (percentage * totalValue) / 100;
             Console.WriteLine(percentage + "percentage of " + totalValue + " is = $" + result);
             Console.ReadLine();
 
+        }
+
+        static void getSquareRoot(Double squareValue)
+        {
+            Double result = 0;
+            result = Math.Sqrt(squareValue);
+            Console.WriteLine(squareValue + " Squre root is = " + result);
+            Console.ReadLine();
 
         }
     }
