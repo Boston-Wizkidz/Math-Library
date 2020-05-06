4f1d4b4dd1aa6723662c8ff6537f3d08df55a069
Adidave2009 <64607962+Adidave2009@users.noreply.github.com>
2020-05-06 10:52:49 -0400

    Added a Addition Calculator.

--- Percentage-Calculator/Percentage-Calculator/Program.cs
+++ Percentage-Calculator/Percentage-Calculator/Program.cs
@@ -22,58 +22,114 @@ namespace Percentage_Calculator
 
 
             //Double value = Convert.ToDouble(args[0]);
-           // getCube(value);
+            // getCube(value);
 
-            Double value  = Convert.ToDouble(args[0]);
-            Double divisorvalue = Convert.ToDouble(args[1]);
-            getdivision(value, divisorvalue);
+            //Double value = Convert.ToDouble(args[0]);
+            //Double divisorvalue = Convert.ToDouble(args[1]);
+            //getdivision(value, divisorvalue);
+
+            //Double factorvalue = Convert.ToDouble(args[0]);
+            //Double value = Convert.ToDouble(args[1]);
+            //getmultiplication(value, factorvalue);
+
+            //Double count = Convert.ToDouble(args[0]);
+            //Double Tablestart = Convert.ToDouble(args[1]);
+            //gettables(count, Tablestart);
+
+            //Double subtractionvalue = Convert.ToDouble(args[0]);
+            //Double value1 = Convert.ToDouble(args[1]);
+            //getdifference(subtractionvalue, value1);
+
+            Double value = Convert.ToDouble(args[0]);
+            Double value1 = Convert.ToDouble(args[1]);
+            getAddition(value, value1);
         }
 
 
         //static void getPercentage(Decimal totalValue, Decimal percentage)
-        //{
-        //Decimal result = 0;
+        // {
+        // Decimal result = 0;
         //result = (percentage * totalValue) / 100;
         //Console.WriteLine(percentage + "percentage of " + totalValue + " is = $" + result);
-        //Console.ReadLine();
+        // Console.ReadLine();
 
-        //}
-
-        //static void getSquareRoot(Double squareValue)
+        // }
+        // static void getSquareRoot(Double squareValue)
         //{
         //Double result = 0;
-        //result = Math.Sqrt(squareValue);
+        // result = Math.Sqrt(squareValue);
         //Console.WriteLine(squareValue + " Squre root is = " + result);
+        // Console.ReadLine();
+
+        //}
+        // static void getCube(Double value)
+        //{
+        // Double result = 0;
+        // result = value * value * value;
+        // Console.WriteLine(" You entered : " + value + " and the cube of " + value + " is =" + result);
         //Console.ReadLine();
 
         //}
+        // static void getdivision(Double value, Double divisorvalue)
+        // {
+        //Double result = 0;
+        // result = (value / divisorvalue);
+        // Console.WriteLine("You questioned me what is  " + value + " divided by " + divisorvalue + " and the answer is = " + result);
+        //Console.ReadLine();
+        //}
+
+        // static void getmultiplication(Double value, Double factorvalue)
+        //{
+        // Double result = 0;
+        //result = (factorvalue * value);
+        // Console.WriteLine("You questioned me what is " + factorvalue + " multiplied by " + value + " and the answer is = " + result);
+        //Console.ReadLine();
+        //}
 
 
-        static void getCube(Double value)
+        //static void gettables(Double count, Double Tablestart)
+        // {
+        //Double Count = 0;
+        //Double TablValue = Tablestart; 
+        //while (count < 10)
+        //{
+        //Console.WriteLine(Tablestart + " X " + count + " = " + TablValue);
+        //TablValue = TablValue + Tablestart;
+        // count = count + 1;
+        //}
+
+
+        static void getdifference(Double subtractvalue, Double value1)
         {
             Double result = 0;
-            result = value * value * value;
-            Console.WriteLine(" You entered : " + value + " and the cube of " + value + " is =" + result);
+            result = (subtractvalue - value1);
+            Console.WriteLine("You questioned me what is " + subtractvalue + " subtracted by " + value1 + " and the answer is = ");
             Console.ReadLine();
 
         }
 
 
-
-
-        static void getdivision(Double value, Double divisorvalue)
+        static void getAddition(Double value, Double value1)
         {
             Double result = 0;
-            result = (value / divisorvalue);
-            Console.WriteLine("You questioned me what is  " + value + " divided by " + divisorvalue + " and the answer is = " + result);
+            result = (value + value1);
+            Console.WriteLine(value + " + " + value1 + " = " + result);
             Console.ReadLine();
-        
-        
+
+
+
         }
-}
-
-
 
 
     }
+
+
+
+
+
+
+
+}
+
+
 
